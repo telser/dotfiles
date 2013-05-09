56ade96e92ecfb28bb42ed83fa92aa9bae0ad146
Trevis <trevis.elser@clickscape.com>
2013-05-09 16:37:06 -0400

    some stuff

--- deb_install.sh
+++ deb_install.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+NAME=$(uname "-n")
+
 # Manipulate repos
 
 # Perform base installation
@@ -12,6 +14,28 @@ echo "Installing main packages";
 sudo apt-get update;
 sudo apt-get install `cat deb_pkgs.txt`;
 
+# Install correct software
+
+case $NAME in
+  "charmy")
+    # Intel graphics drivers
+    sudo apt-get install xserver-xorg-video-intel
+    # Charmy gets the other default softs/dotfiles
+    ;;
+  "shadow")
+    # Nvidia graphics drivers
+    sudo apt-get install xserver-xorg-video-nvidia
+    # Shadow gets the other default softs/dotfiles
+    ;;
+  *)
+    # Who is this??
+    echo "unkown system installing video-all"
+    sudo apt-get install xserver-xorg-video-all
+    # skipping default softs
+    echo "Skipping non-packaged software. Proceeding to dotfiles"
+    # Still want dotfiles for applications
+    ;;
+esac
 
 # Install "non-free" softs
 
