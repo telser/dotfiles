2f68dd03e258458d865ffed04ffcf131e11ca4c3
Trev <trev311@gmail.com>
2013-05-09 22:26:30 -0400

    merge

--- deb_install.sh
+++ deb_install.sh
@@ -6,13 +6,29 @@ set -e
 
 # Manipulate repos
 
+sudo sed 's/jessie/testing/' </etc/apt/sources.list >/etc/apt/sources.list
+sudo echo "deb http://repository.spotify.com/ stable non-free" >> /etc/apt/sources.list
+
 # Perform base installation
 
 echo "Installing main packages";
 sudo apt-get update;
+sudo apt-get upgrade;
 sudo apt-get install `cat deb_pkgs.txt`;
 
+sudo update-command-not-found;
 
-# Install "non-free" softs
 
 # Dotfiles!
+
+# Install "non-free" softs from outside repos
+
+# Steam
+
+wget http://media.steampowered.com/client/installer/steam.deb
+
+# FL Studio
+wget demodownload.image-line.com/flstudio/flstudio_11.exe
+
+
+
