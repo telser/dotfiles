45c5e9ae1ab413ff4ed458955d3ab881d05ce7f8
Trev <trev311@gmail.com>
2014-01-15 14:32:43 -0500

    from work

--- .zshrc
+++ .zshrc
@@ -5,7 +5,12 @@ ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="muse"
+if [ -n "$INSIDE_EMACS" ]; then
+   ZSH_THEME="candy"
+   unsetopt zle
+else
+   ZSH_THEME="muse"
+fi
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
--- emacs/personal.el
+++ emacs/personal.el
@@ -8,9 +8,13 @@
 (custom-set-variables
  '(haskell-mode-hook '(turn-on-haskell-indentation)))
 
+(setq nrepl-hide-special-buffers t)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-to-list 'custom-theme-load-path "~/emacs")
+
+(setenv "ESHELL" (expand-file-name "~/bin/eshell"))
 ;;(color-theme-ir-black-chap)
 (setq system-uses-terminfo nil)
 (provide 'personal)
