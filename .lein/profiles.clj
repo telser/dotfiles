3502fc9627cff5c71544e053b1067a996b22d2f9
Trev <telser@emory.edu>
2014-07-27 11:05:58 -0400

    nrepl, php dev etc etc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,8 @@
             [lein-depgraph "0.1.0"]
             [lein-ancient "0.5.5"]
             [jonase/eastwood "0.1.2"]
-            [lein-kibit "0.0.8"]]
+            [lein-kibit "0.0.8"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
   :eastwood {:add-linters [:unused-fn-args :unused-namespaces]}
   :env {:servo-conf "/Users/trevis/work/tom-servo/dev.conf"}
   :deploy-repositories
