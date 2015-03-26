00a2a27ad86238ce7bae507318b6216a8a6ef554
Trev <telser@emory.edu>
2015-03-26 15:50:25 -0400

    Some emacs updates

--- emacs/ercrc.el
+++ emacs/ercrc.el
@@ -1,11 +1,11 @@
 (require 'erc)
 
-(erc :server "irc.freenode.net" :port "6667" :nick "telser"
-     :full-name "telser")
-(erc-tls :server "irc.lambdaphil.es" :port "9999" :nick "telser"
-	:full-name "telser")
+;; (erc :server "irc.freenode.net" :port "6667" :nick "telser"
+;;      :full-name "telser")
+;; (erc-tls :server "irc.lambdaphil.es" :port "9999" :nick "telser"
+;; 	:full-name "telser")
 
-(setq erc-autojoin-channels-alist '(("freenode.net" "#clojure" "#elm" "#erlang" "#haskell" "#typed-clojure")
-                                    ("lambdaphil.es" "#lambdaphiles" "#dev")))
+;; (setq erc-autojoin-channels-alist '(("freenode.net" "#clojure" "#elm" "#erlang" "#haskell" "#typed-clojure")
+;;                                     ("lambdaphil.es" "#lambdaphiles" "#dev")))
 
 (setq erc-prompt (lambda () (concat "[" (buffer-name) "]")))
