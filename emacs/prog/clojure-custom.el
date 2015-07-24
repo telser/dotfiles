1fa712099c1df805d131e6e43a8b6b368ff76dbb
telser <trevis@clickscape.com>
2015-07-24 10:46:07 -0400

    Emacs updates

--- emacs/prog/clojure-custom.el
+++ emacs/prog/clojure-custom.el
@@ -1,4 +1,11 @@
-;;;; Clojure mode setup
+;;; Package --- Summary:
+
+
+;;;; Clojure mode
+
+;;; Commentary:
+
+;;; Code:
 
 (require-packages '(clojure-mode cider typed-clojure-mode
                                  clojure-snippets clojurescript-mode))
@@ -8,11 +15,13 @@
 (require 'rainbow-delimiters)
 
 (defun clojure-mode-setup ()
+  "Clojure."
   (electric-indent-mode +1)
   (subword-mode +1)
   (rainbow-delimiters-mode +1))
 
 (defun cider-mode-setup ()
+  "Cider mode stuff."
   (rainbow-delimiters-mode +1)
   (subword-mode +1)
   (whitespace-mode -1))
@@ -20,3 +29,4 @@
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
 (add-hook 'clojure-mode-hook 'clojure-mode-setup)
 (add-hook 'cider-repl-mode-hook 'cider-mode-setup)
+;;; clojure-custom.el ends here
