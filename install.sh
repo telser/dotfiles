0c0e66a66a45ae1c2bf8066a0face4d60b686891
telser <trevis.elser@gmail.com>
2016-09-16 21:52:10 -0400

    serverconfs and installsh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/.xinitrc ~/.xinitrc
+ln -s ~/dotfiles/.spacemacs ~/.spacemacs
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
+ln -s ~/dotfiles/.xmonad ~/.xmonad
+ln -s ~/dotfiles/tint2.conf ~/tint2.conf
+ln -s ~/dotfiles/.conkyLeftrc ~/.conkyLeftrc
+ln -s ~/dotfiles/.conkyRightrc ~/.conkyRightrc
