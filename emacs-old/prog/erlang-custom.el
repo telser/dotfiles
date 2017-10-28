af7c1774e7e1e3251d06bb42b12bde6eb89a8c53
Trevis <trev311@gmail.com>
2017-10-27 21:01:26 -0400

    Removing old files

--- emacs-old/prog/erlang-custom.el
+++
@@ -1,9 +0,0 @@
-;;; Erlang
-
-
-;;; Code:
-
-(require-packages '(erlang edts))
-(add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
-(add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
-;;; Erlang-custom ends here
